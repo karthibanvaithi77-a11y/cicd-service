5d3e1a9702008aa44310860df69125ab2fff6dbe
karthibanvaithi <karthibanvaithi77@gmail.com>
2026-03-08 09:38:55 +0530

    Test case added

--- src/test/java/com/example/cicd_service/CicdServiceApplicationTests.java
+++ src/test/java/com/example/cicd_service/CicdServiceApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.cicd_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CicdServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+        OrderController order = new OrderController();
+        assertEquals("Order Service Running" , order.getOrders());
 	}
 
 }
